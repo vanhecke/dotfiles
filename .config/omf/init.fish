67175eebcd5c1fbfe24c7f6a86f3fecda9c1b3f0
Joris Vanhecke <joris@jorisvanhecke.be>
2015-10-01 16:10:42 +0200

    Set TextMate as the $EDITOR

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -2,3 +2,5 @@ set HOMEBREW_CASK_OPTS '--appdir=/Applications'
 
 # set the gnu coreutils as default in stead of OSX's outdated stuff.
 set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+
+set EDITOR '/usr/local/bin/mate -w'
